dc26dd4bdc07d109c53e18df1289ee92e7aaa663
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2022-06-12 15:13:28 +0900

    7장 배열

--- 3부 선형자료구조/7장 배열/12 주식을 사고팔기 가장 좋은 지점/my.py
+++ 3부 선형자료구조/7장 배열/12 주식을 사고팔기 가장 좋은 지점/my.py
@@ -2,5 +2,16 @@
 
 nums: list[int] = [7, 1, 5 ,3 ,6, 4]
 
-nums = [2, 9, 1, 4]
+my_dict = dict() 
+for index,value in enumerate(nums):
+  my_dict[value] = index
+
+my_dict = sorted(my_dict.items())
+
+
+
+print(my_dict[0])
+print(my_dict[len(my_dict)])
+
+
 
--- 3부 선형자료구조/7장 배열/12 주식을 사고팔기 가장 좋은 지점/풀이2 저점과 현재값과의 차이 계산.py
+++ 3부 선형자료구조/7장 배열/12 주식을 사고팔기 가장 좋은 지점/풀이2 저점과 현재값과의 차이 계산.py
@@ -4,8 +4,6 @@ prices: list[int] = [7, 1, 5 ,3 ,6, 4]
 profit: int = 0
 min_price: int = sys.maxsize
 
-prices = [2, 9, 1, 4]
-
 for price in prices:
   min_price = min(min_price, price)
   profit = max(profit, price - min_price)
