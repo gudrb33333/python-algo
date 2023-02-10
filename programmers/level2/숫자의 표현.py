93fc184c978c7ef36ee6c92f9b6adfe351df26d6
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2023-02-10 10:01:59 +0900

    Create 숫자의 표현.py

---
+++ programmers/level2/숫자의 표현.py
@@ -0,0 +1,15 @@
+def solution(n):
+    answer = 0
+    
+    for i in range(1,n + 1):
+        acc = 0
+        for j in range(i, n + 1):
+            acc += j
+            if acc > n:
+                break
+            
+            if acc == n:
+                answer += 1
+                break
+    
+    return answer
